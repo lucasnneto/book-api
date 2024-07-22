29d89c87d7a597c299ae883b54cf148161ab8298
Lucas Neto <lucasn-oliveira@hotmail.com>
2024-07-22 19:34:15 -0300

    fix: remoção da portal base

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
 
 // Inicialização do servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
